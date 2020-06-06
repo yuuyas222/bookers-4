2ddeb9aab911b3ca85b72fdf65eef4e0f516d967
佐藤　祐弥 <y.satou@satouyuuwatarunoMacBook-Air.local>
2020-06-06 17:00:34 +0900

    Fix content

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -12,12 +12,12 @@ class RelationshipsController < ApplicationController
   def create
     follow = current_user.active_relationships.new(follower_id: params[:user_id])
     follow.save
-    redirect_to users_path
+    redirect_back(fallback_location: request.path)
   end
 
   def destroy
     follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
     follow.destroy
-    redirect_to users_path
+    redirect_back(fallback_location: request.path)
   end
 end
